6ef59a6b4912cb4184f7add1a45646fa32406ecd
Cristian Camilo Benitez Peña <78176029+CristianCamiloBenitez@users.noreply.github.com>
2023-10-03 22:59:00 -0500

    CORS

--- taller/src/components/List.vue
+++ taller/src/components/List.vue
@@ -9,57 +9,63 @@
             <th>ID</th>
             <th>Nombre Tarea</th>
             <th>Descripción</th>
+            <th>Completada</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="item in listaTareas" :key="item.id">
             <td>{{ item.id }}</td>
-            <td>{{ item.name }}</td>
-            <td>{{ item.username }}</td>
+            <td>{{ item.title }}</td>
+            <td>{{ item.description }}</td>
+            <td>{{ item.completed }}</td>
           </tr>
         </tbody>
     </table>
 </template>
 
 <script>
-    export default {
-        name: 'List',
-        data () {
-            return {
-                listaTareas: []
-            }
-        },
-        methods: {
-            async getTareas () {
-                const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-                //('http://localhost:8080/read/task')
-                this.listaTareas = response.data
-            }
-        },
-        created () {
-            this.getTareas()
-        }
-    }
+import axios from 'axios';
 
-    import axios from 'axios';
+export default {
+    name: 'List',
+    data () {
+        return {
+            listaTareas: []
+        }
+    },
+    methods: {
+        async getTareas () {
+            try {
+                const response = await axios.get('http://localhost:8081/read/tasks/get_all');
+                console.log(response.data);
+                this.listaTareas = response.data; // Asume que la respuesta es un array de objetos JSON
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    },
+    created () {
+        this.getTareas();
+    }
+}
 </script>
 
 <style>
-    @import '../assets/css/bootstrap.css';
+@import '../assets/css/bootstrap.css';
 
-    table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-    }
+table {
+font-family: arial, sans-serif;
+border-collapse: collapse;
+width: 100%;
+}
 
-    td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px;
-    }
+td, th {
+border: 1px solid #dddddd;
+text-align: left;
+padding: 8px;
+}
 
-    tr:nth-child(even) {
-    background-color: #dddddd;
-    }
+tr:nth-child(even) {
+background-color: #dddddd;
+}
 </style>
